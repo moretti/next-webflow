29c81715aaaead4e166673b3cd5671937f15c2f3
Murat Mutlu <murat@marvelapp.com>
2022-10-27 00:21:52 +0100

    Update [...path].tsx

--- src/pages/[...path].tsx
+++ src/pages/[...path].tsx
@@ -7,7 +7,7 @@ export { getStaticProps };
 export async function getStaticPaths() {
   return {
     paths: [
-      { params: { path: ['landing', 'landing-3'] } },
+      { params: { path: ['landing', 'landing-2'] } },
       { params: { path: ['landing', 'solutions'] } },
       { params: { path: ['ui-snippets'] } },
             { params: { path: ['landing-2'] } },
