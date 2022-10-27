68d3b022078fd67ded5f587d0275a1c9e97613db
Murat Mutlu <murat@marvelapp.com>
2022-10-27 14:13:55 +0100

    Update [...path].tsx

--- src/pages/[...path].tsx
+++ src/pages/[...path].tsx
@@ -7,10 +7,10 @@ export { getStaticProps };
 export async function getStaticPaths() {
   return {
     paths: [
-      { params: { path: ['landing', 'landing-2'] } },
-      { params: { path: ['landing', 'solutions'] } },
+      { params: { path: ['solutions', 'design-survey'] } },
+      { params: { path: ['solutions', 'user-testing-for-figma-prototypes'] } },
       { params: { path: ['ui-snippets'] } },
-            { params: { path: ['landing-2'] } },
+      { params: { path: ['landing', 'landing-2'] } },
     ],
     fallback: `blocking`,
   };
